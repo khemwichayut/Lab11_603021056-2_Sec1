b688881dcb289b66556d81cf6a26be1558214299
san4077 <k_siriwatsann@kkumail.com>
2019-10-28 16:09:36 +0700

    Lab11_603021056-2_Sec1

---
+++ app/build/generated/source/buildConfig/release/com/example/lab11apk/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.lab11apk;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.lab11apk";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
